e61b6dbc7e68eb56d51f0730ca375370a04fc606
augustin <54428095+augustin221@users.noreply.github.com>
2020-06-13 13:26:52 +0300

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <array>
+#include  <cmath>
+#include <locale>
+#include <vector>
+using namespace std;
+
+
+int main() {
+	double dX(0.0), dY(0.0) ,dZ(0.0);
+	double dVectorLength(0.0);
+	
+	cout << "залупа";
+	cin >> dX;
+	cout << "вова пидор";
+	cin >> dY;
+	cout << "иди нахуй";
+	cin >> dZ;
+	
+	cout << "\n веели следушие числа" << dX << "боять" << dY << dX;
+	
+	dVectorLength = sqrt(dX*dY*dZ);
+
+	cout << "\n длина вектора" << dVectorLength << "\n залупа ";
+	
+	return 0;
+	
+}
